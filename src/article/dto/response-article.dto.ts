e01fdc048fe8edba9ae4ef4c554ec88919d80bd5
Glory-Mmachi <Mglory360@gmail.com>
2025-08-29 22:09:56 +0100

    modified dto

--- src/article/dto/response-article.dto.ts
+++ src/article/dto/response-article.dto.ts
@@ -1,18 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class ResponseArticleDto {
   @ApiProperty()
+  @Expose()
   id: string;
 
   @ApiProperty()
+  @Expose()
   title: string;
 
   @ApiProperty()
+  @Expose()
   content: string;
 
   @ApiProperty()
+  @Expose()
   createdAt: Date;
+
   @ApiProperty()
+  @Expose()
   updatedAt: Date;
 
   constructor(partial: Partial<ResponseArticleDto>) {
