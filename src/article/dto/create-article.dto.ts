0f633bbfe0488bfa858635aeba3c1822c588ec20
Glory-Mmachi <Mglory360@gmail.com>
2025-08-29 21:56:57 +0100

    modified create-article.dto

--- src/article/dto/create-article.dto.ts
+++ src/article/dto/create-article.dto.ts
@@ -1,31 +1,27 @@
-import { IsString, MinLength, MaxLength } from 'class-validator';
+import { IsString, MaxLength, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export const TITLE_MIN_LENGTH = 3;
 export const TITLE_MAX_LENGTH = 200;
-export const CONTENT_MIN_LENGTH = 10;
 export const CONTENT_MAX_LENGTH = 2000;
 
 export class CreateArticleDto {
   @ApiProperty({
     description: 'Article name',
     example: 'My First blog',
-    minLength: TITLE_MIN_LENGTH,
     maxLength: TITLE_MAX_LENGTH,
   })
   @IsString()
-  @MinLength(TITLE_MIN_LENGTH, { message: 'Title is too short' })
+  @IsNotEmpty({ message: 'Title cannot be empty' })
   @MaxLength(TITLE_MAX_LENGTH, { message: 'Title is too long' })
   title: string;
 
   @ApiProperty({
     description: 'Article content',
     example: 'This is the content of my first blog',
-    minLength: CONTENT_MIN_LENGTH,
     maxLength: CONTENT_MAX_LENGTH,
   })
   @IsString()
-  @MinLength(CONTENT_MIN_LENGTH, { message: 'Content is too short' })
+  @IsNotEmpty({ message: 'Content cannot be empty' })
   @MaxLength(CONTENT_MAX_LENGTH, { message: 'Content is too long' })
   content: string;
 }
